2373e1d2e335a227997b51eb7280f016d7586925
amit02-ux <97420469+amit02-ux@users.noreply.github.com>
2024-06-07 22:31:05 +0530

    server created

---
+++ server.js
@@ -0,0 +1,23 @@
+const express=require('express')
+const cors=require('cors')
+const morgan=require('morgan')
+const dotenv=require('dotenv')
+const colors=require('colors')
+dotenv.config()
+// rest object 
+const app=express()
+// middleware
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(cors())
+app.get('/',(req,res)=>{
+    res.send("<h1>Hello from amit</h1>"
+
+    )
+})
+const PORT=8080|| process.env.PORT
+//listen
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${
+        PORT}`)
+})
