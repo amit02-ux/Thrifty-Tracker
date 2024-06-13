1e7c4989a4ed6a10e86176b4c76ed4532018024b
amit02-ux <97420469+amit02-ux@users.noreply.github.com>
2024-06-13 11:59:49 +0530

    layout

---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const Homepage = () => {
+  return (
+    <Layout>
+        <h1>Expence tracking System</h1>
+    </Layout>
+  )
+}
+
+export default Homepage
