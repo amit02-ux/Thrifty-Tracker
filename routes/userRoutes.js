e9c02af8588d76b60ed80d216912fe3032755bda
amit02-ux <97420469+amit02-ux@users.noreply.github.com>
2024-08-18 10:57:35 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express=require('express')
+const authMiddlewares = require("../middlewares/authMiddlewares.js");
 const   {loginController,registerController,forgot_passwordController,reset_passwordController,change_passwordController,reset1_passwordController}=require("../controllers/userController");
 const router=express.Router()
 router.post('/login',loginController)
 router.post('/register',registerController)
 router.post('/forgot_password',forgot_passwordController)
 router.get('/reset_password/:token',reset_passwordController)
-router.post('/change_password/:token',change_passwordController)
+router.post('/change_password/:token',authMiddlewares,change_passwordController)
 router.post('/reset1_password',reset1_passwordController)
 module.exports=router
 
